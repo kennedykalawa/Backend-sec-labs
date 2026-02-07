f999d55372c474964c4600aaa499976e6eb7e811
kennedykalawa <kalawamkennedy@gmail.com>
2026-02-07 13:39:10 +0300

    .

---
+++ projects/learning/Js/first.js
@@ -0,0 +1,17 @@
+console.log("Hello Ktec Family👋🏽");
+
+var a;
+var b = 2;
+
+a = 6
+console.log(a);
+console.log(b + 1);
+//operations
+var c = 5;
+var d = 6.5;
+
+c++;
+console.log(c)
+c--;
+console.log(c)
+ 
